b6891722d247dc22d5ec4c3093b3aaf1509d3fc1
Tamas Molnar <mtomiwork@gmail.com>
2024-02-09 17:10:23 +0100

    ACME Projectfeladat - landing js script hiba javítása, js file-ok és nav kezelés javítása

---
+++ src/js/footer.js
@@ -0,0 +1,8 @@
+/*Footer content kód */ 
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    if (document.querySelector('.footer-content')) {
+        document.querySelector('.footer-content').innerHTML = '<div><span>ACME</span></div>';
+    }
+});
